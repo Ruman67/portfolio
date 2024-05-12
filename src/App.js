fd5116c987157b957bbdfdf5b40baa485f509063
Ruman67 <126407391+Ruman67@users.noreply.github.com>
2024-05-12 19:26:58 +0545

    Created Navbar

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar/Navbar';
+function App() {
+	return (
+		<>
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" element="Home" />
+					<Route path="/services" element="Services" />
+					<Route path="/about" element="About" />
+					<Route path="/contact" element="Contact" />
+				</Routes>
+			</Router>
+		</>
+	);
+}
+
+export default App;
