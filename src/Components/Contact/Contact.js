b0c87224f3fa9f9228e1175160cfa530c2aa4d93
Ruman67 <126407391+Ruman67@users.noreply.github.com>
2024-05-20 22:39:14 +0545

    contact updated

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,14 +1,15 @@
 // Components/Contact/Contact.js
 import React from 'react';
+
 function Contact() {
     return (
-        <div>
-            <h1>Contact Us</h1>
+        <div style={{ textAlign: 'center' }}>
+            <h1>Contact Me</h1>
             <p>If you have any questions, feel free to reach out to us:</p>
-            <ul>
+            <ul style={{ listStyle: 'none', padding: 0 }}>
                 <li>Email: rumanmaharjan770@gmail.com</li>
-                <li>Phone:9861727868</li>
-                <li>Address: 123 Main Street, Anytown, USA</li>
+                <li>Phone: 9861727868</li>
+                <li>Address: Mahalaxmi 8, Lalitpur</li>
             </ul>
             <p>We look forward to hearing from you!</p>
         </div>
